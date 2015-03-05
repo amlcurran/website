45b77672f02c9852d87bd17b41ff9e497334a1a2
Alex Curran <aml.curran@gmail.com>
2015-03-05 19:39:55 +0000

    Fade in chips when loaded

--- chips.js
+++ chips.js
@@ -22,11 +22,17 @@ function ChipsView () {
 ChipsView.prototype.renderChips = function(chips) {
 
     var chipsHolder = document.querySelector('#chips_holder');
+    var chipsInner = document.createElement('div');
     for (i = 0; i < chips.length; i++) {
       var chip = chips[i];
       chipsHolder.appendChild(this.createChip(chip));
     }
+    // chipsHolder.appendChild(chipsInner);
 
+}
+
+ChipsView.prototype.fadeIn = function(element) {
+  element.className = element.className + ' fadeIn';
 }
 
 ChipsView.prototype.createChip = function(chip) {
@@ -39,7 +45,8 @@ ChipsView.prototype.createChip = function(chip) {
   var image = document.createElement('img');
   image.title = chip.title;
   image.className = image.className + ' chip';
-  image.srcset = this.imageUrl(chip);
+  image.src = this.imageUrl(chip);
+  image.addEventListener('load', this.fadeIn(image));
   image.style.color = chip.color;
 
   a.appendChild(image);
