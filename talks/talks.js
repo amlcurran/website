5b09e6de262dd71760935ecda7afcf623d85d78b
Alex Curran <aml.curran@gmail.com>
2015-04-18 10:32:44 +0100

    Use polyfill for HTMLImports

--- talks/talks.js
+++ talks/talks.js
@@ -1,26 +1,43 @@
 this.talksView = function() {
   
   var talksHolder = document.querySelector('#talks-holder');
-  var talksChunkBase = document.querySelector('#talk-chunk-import').import;
+  
+  setText = function(node, text) {
+    node.innerText = text;  
+    node.textContent = text;  
+  };
   
   createTalkItem = function(talk) {
-    var talkDiv = talksChunkBase.querySelector('#talk-chunk').cloneNode(true);
-    talkDiv.querySelector('#talk-title').innerText = talk.title;
-    talkDiv.querySelector('#talk-blurb').innerText = talk.blurb;
-    talkDiv.querySelector('#talk-link').innerText = talk.online.place;
+    var talkImport = document.querySelector('#talk-chunk-import').import;
+    var talkDiv = talkImport.querySelector('#talk-chunk').cloneNode(true);
+    setText(talkDiv.querySelector('#talk-title'), talk.title);
+    setText(talkDiv.querySelector('#talk-blurb'), talk.blurb);
+    setText(talkDiv.querySelector('#talk-link'), talk.online.place);
     talkDiv.querySelector('#talk-link').href = talk.online.url;
     talkDiv.querySelector('#talk-image').style.backgroundImage = 'url("talks/' + talk.image + '")';
     talkDiv.querySelector('#talk-dl').href = talk.online.dl;
     return talkDiv;
   };
   
+  addViews = function(talks) {
+    for (i = 0; i < talks.length; i++) {
+      var talk = talks[i];
+      talksHolder.appendChild(createTalkItem(talk));
+    }   
+  };
+  
   return {
     
     loaded : function(talks) {
-        for (i = 0; i < talks.length; i++) {
-          var talk = talks[i];
-          talksHolder.appendChild(createTalkItem(talk));
-        } 
+      
+      if (document.querySelector('#talk-chunk-import').import) {
+        addViews(talks); 
+      } else {
+        window.addEventListener('HTMLImportsLoaded', function() {
+          addViews(talks);  
+        });
+      }
+      
     }
     
   };
