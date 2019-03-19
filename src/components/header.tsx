189cd18aae76a32d701476822e1b5bb16fa34112
amlcurran <aml.curran@gmail.com>
2019-03-19 21:29:47 +0100

    Adds a dot to current page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,23 +33,32 @@ const externalLinkStyle: CSSProperties = {
 interface HeaderLinkProps {
   to: string
   text: string
+  current: Location
 }
 
-const HeaderLink = ({to, text}: HeaderLinkProps) => (
+const HeaderLink = ({to, text, current}: HeaderLinkProps) => (
   <Link to={to} style={{flexGrow: 1}}>
-    <div style={linkInnerStyle}>{text}</div>
+    <div style={linkInnerStyle}>{dotIfCurrent(to, current) + text}</div>
   </Link>
 )
+
+function dotIfCurrent(to: string, current: Location): string {
+  if (current.href.indexOf(to) != -1) {
+    return "● "
+  } else {
+    return ""
+  }
+}
 
 const Header = () => (
   <header>
     <div style={{...contentStyle, ...myStyle}}>I am Alex Curran.</div>
     <div style={linkHostStyle} className="emphasisBox">
       <div style={{...contentStyle, ...linkHostInnerStyle}}>
-        <HeaderLink to="/" text="Home" />
-        <HeaderLink to="/portfolio" text="Portfolio" />
-        <HeaderLink to="/talks" text="Talks" />
-        <HeaderLink to="/not-tech" text="Not tech" />
+        <HeaderLink to="/" text="Home" current={window.location} />
+        <HeaderLink to="/portfolio" text="Portfolio" current={window.location} />
+        <HeaderLink to="/talks" text="Talks" current={window.location} />
+        <HeaderLink to="/not-tech" text="Not tech" current={window.location} />
         <a href="https://www.medium.com/@amlcurran" style={externalLinkStyle} target="_blank">Blog</a>
       </div>
     </div>
