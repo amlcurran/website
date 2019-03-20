fd310e8229922968ab0fa5b1930f649e628568ac
amlcurran <aml.curran@gmail.com>
2019-03-20 21:17:36 +0100

    Improve the portfolio badges

---
+++ src/components/badge.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties } from "react";
+
+const badgeStyle: CSSProperties = {
+    display: "flex", 
+    flexDirection: "column", 
+    alignItems: "center", 
+    justifyContent: "space-around", 
+    marginLeft: 16, 
+    marginTop: 8,
+    fontSize: 12,
+    textTransform: "uppercase",
+    fontWeight: "bold"
+}
+
+interface BadgeProps {
+    text: string
+    component: JSX.Element
+}
+
+const Badge = ({text, component}: BadgeProps) => {
+    return (
+        <div style={badgeStyle}>
+            {component}
+            {text}
+        </div>
+    )
+}
+
+export default Badge
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { Component } from "react"
 import { graphql } from "gatsby"
 
+import Badge from "../components/badge"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AppleLogo from "../images/apple_logo.svg"
@@ -51,14 +52,13 @@ const Portfolio = ({ data }: { data: PortfolioQuery }) => {
 function asPortfolioExcerpt({ node }: { node: MarkdownRemark }): JSX.Element {
     return (
         <div key={node.frontmatter.title}>
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex', marginBottom: 24 }}>
                 <div style={{ flexGrow: 1 }}>
                     <h5>{node.frontmatter.date}</h5>
-                    <h3>{node.frontmatter.title}</h3>
+                    <h3 style={{marginBottom: 0}}>{node.frontmatter.title}</h3>
                 </div>
-                <div>
-                    {node.frontmatter.team} ● {platforms(node)}
-                </div>
+                <Badge text="Devs" component={(<div style={{fontWeight: 700, fontSize: 20}}>{node.frontmatter.team}</div>)} />
+                {platforms(node)}
             </div>
             <div dangerouslySetInnerHTML={{ __html: node.html }} />
         </div>
@@ -66,8 +66,8 @@ function asPortfolioExcerpt({ node }: { node: MarkdownRemark }): JSX.Element {
 }
 
 function platforms(node: MarkdownRemark): JSX.Element {
-    const apple = node.frontmatter.platforms.includes("iOS") ? <AppleLogo style={{ fill: "white" }} /> : null
-    const android = node.frontmatter.platforms.includes("android") ? <AndroidLogo height="24px" style={{ fill: "white" }} /> : null
+    const apple = node.frontmatter.platforms.includes("iOS") ? <Badge text="iOS" component={<AppleLogo style={{ fill: "white" }} />} /> : null
+    const android = node.frontmatter.platforms.includes("android") ? <Badge text="Android" component={<AndroidLogo height="24px" style={{ fill: "white" }} />} /> : null
     return (<>
         {apple}
         {android}
