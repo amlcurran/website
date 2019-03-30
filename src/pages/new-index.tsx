0323716024e478ffbfa7758e8e6670a793174d59
Alex Curran <alex@novoda.com>
2019-03-30 14:07:24 +0000

    Adds initial new index page

---
+++ src/pages/new-index.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { MarkdownRemark } from "../models/remark";
+
+const IndexPage = ({ data }: IndexQuery) => {
+  return (
+    <Layout seo={<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} description="Alex Curran's portfolio website" />}>
+      {[<div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />]}
+    </Layout>
+  )
+}
+
+interface IndexQuery {
+  data: { markdownRemark: MarkdownRemark<any> }
+}
+
+export const pageQuery = graphql`
+  query {
+    markdownRemark(fileAbsolutePath: { glob: "**/home.md" }) {
+        html 
+    }
+  }
+`
+
+export default IndexPage
