80c53f2f6401ba1c5210b4ef267154d96a737ca8
Alex Curran <alex@novoda.com>
2019-04-04 17:55:22 +0200

    Load backen iphone screen

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -4,7 +4,6 @@ import { graphql } from "gatsby"
 import Badge from "../components/badge"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Img, { FluidObject } from "gatsby-image";
 import AppleLogo from "../images/apple_logo.svg"
 import AndroidLogo from "../images/android.svg"
 import { GraphQLList, Edge } from "../models/graphql";
@@ -24,11 +23,10 @@ interface PortfolioFrontmatter {
 
 interface PortfolioQuery {
   allMarkdownRemark: GraphQLList<MarkdownRemark<PortfolioFrontmatter>>
-  demo: { childImageSharp: { fluid: FluidObject } }
 }
 
 const Portfolio = ({ data }: { data: PortfolioQuery }) => {
-  const elements = data.allMarkdownRemark.edges.map(asPortfolioExcerpt(data.demo))
+  const elements = data.allMarkdownRemark.edges.map(asPortfolioExcerpt)
   const seo = <SEO title="Portfolio" keywords={[`portfolio`, `developer`, `engineer`, `mobile`, `ios`, `android`]} description="A series of my most popular projects" key="SEO" />
   return (
     <Layout seo={seo}>
@@ -37,23 +35,21 @@ const Portfolio = ({ data }: { data: PortfolioQuery }) => {
   )
 }
 
-function asPortfolioExcerpt(demo: { childImageSharp: { fluid: FluidObject } }): ({ node }: Edge<MarkdownRemark<PortfolioFrontmatter>>) => JSX.Element {
-  return ({ node }) => {
-    const badges = [
-      <Badge key="devs" text="Devs" component={(<div style={{ fontWeight: 700, fontSize: 20, color: Styling.white }}>{node.frontmatter.team}</div>)} />,
-      platforms(node.frontmatter.platforms)
-    ]
-    return (
-      <LargeCard
-        key={node.frontmatter.title}
-        title={node.frontmatter.title}
-        date={node.frontmatter.date}
-        badges={badges}
-        html={node.html}
-        with={node.frontmatter.with}
-        image={<BackenIPhone />} />
-    )
-  }
+function asPortfolioExcerpt({ node }: Edge<MarkdownRemark<PortfolioFrontmatter>>): JSX.Element {
+  const badges = [
+    <Badge key="devs" text="Devs" component={(<div style={{ fontWeight: 700, fontSize: 20, color: Styling.white }}>{node.frontmatter.team}</div>)} />,
+    platforms(node.frontmatter.platforms)
+  ]
+  return (
+    <LargeCard
+      key={node.frontmatter.title}
+      title={node.frontmatter.title}
+      date={node.frontmatter.date}
+      badges={badges}
+      html={node.html}
+      with={node.frontmatter.with}
+      image={<BackenIPhone />} />
+  )
 }
 
 function platforms(platforms: string[]): JSX.Element {
@@ -81,13 +77,6 @@ export const pageQuery = graphql`{
         }
       }
     }
-    demo: file(relativePath: { eq: "iphone-x.jpeg" }) {
-        childImageSharp {
-          fluid(maxWidth: 600) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
   }
 `
 
