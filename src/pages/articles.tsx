79bbccd3f1b2ed9b1efa59f02df8f80b84c48a64
amlcurran <aml.curran@gmail.com>
2019-04-13 23:39:48 +0200

    Adds a basic articles page

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -11,8 +11,8 @@ const Articles = ({ data }: { data: ArticlesQuery }) => {
     const seo = <SEO title="Not tech" keywords={[`articles`, `blog`, `vlog`, `tech`, `thoughts`]} description="Articles and piece I've written" key="SEO"/>
     return (
         <Layout seo={seo}>
-            This part of my website will be coming soon...
-            {}
+            <p>All my new articles will be released here, but right now there aren't any. </p>
+            <p>Check my <a href="https://www.medium.com/@amlcurran" target="_blank">Medium</a> account for older ones.</p>
         </Layout>
     )
 }
