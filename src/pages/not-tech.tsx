42bbc4ef6b3ec5927922e51646af416b3fe8b82f
amlcurran <aml.curran@gmail.com>
2019-04-15 22:31:49 +0200

    Adds all the images to not tech page

--- src/pages/not-tech.tsx
+++ src/pages/not-tech.tsx
@@ -4,17 +4,40 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
+import { GraphQLList } from "../models/graphql";
 
 interface NotTechQuery {
-
+    allFile: GraphQLList<any>
 }
 
 const NotTech = ({ data }: { data: NotTechQuery }) => {
+    const rounds = (data.allFile.edges.length / 3)
+    const elements: JSX.Element[] = []
+    for (let i = 0; i < rounds; i++) {
+        const foo = i * 3
+        const images = data.allFile.edges.slice(foo, foo + 3)
+        elements.push(
+            <div style={{display: "flex", position: "relative"}}>
+                <Img fluid={images[0].node.childImageSharp.fluid} style={{flexBasis: '60%'}} />
+                <div style={{display: "flex", flexDirection: "column", flexBasis: '40%'}}>
+                    <Img fluid={images[1].node.childImageSharp.fluid} style={{flexBasis: '50%'}} />
+                    {final(images)}
+                </div>
+            </div>
+        )
+    }
     return (
         <Layout seo={seo}>
-            {data.allFile.edges.map(edge => <Img fluid={edge.node.childImageSharp.fluid} />)}
+            {elements}
         </Layout>
     )
+}
+
+function final(images: any): JSX.Element | null {
+    if (images[2]) {
+        return <Img fluid={images[2].node.childImageSharp.fluid} style={{flexBasis: '50%'}} />                 
+    }
+    return null;
 }
 
 const seo = <SEO
