02e0befb10424ceadac4336d3588e02fa261f80a
amlcurran <aml.curran@gmail.com>
2019-04-16 21:51:40 +0200

    Show images in not tech

--- src/pages/not-tech.tsx
+++ src/pages/not-tech.tsx
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { CSSProperties } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 import { GraphQLList } from "../models/graphql";
+
+const imageStyle: CSSProperties = {
+}
 
 interface NotTechQuery {
     allFile: GraphQLList<any>
@@ -14,17 +17,9 @@ const NotTech = ({ data }: { data: NotTechQuery }) => {
     const rounds = (data.allFile.edges.length / 3)
     const elements: JSX.Element[] = []
     for (let i = 0; i < rounds; i++) {
-        const foo = i * 3
-        const images = data.allFile.edges.slice(foo, foo + 3)
-        elements.push(
-            <div style={{display: "flex", position: "relative"}}>
-                <Img fluid={images[0].node.childImageSharp.fluid} style={{flexBasis: '60%'}} />
-                <div style={{display: "flex", flexDirection: "column", flexBasis: '40%'}}>
-                    <Img fluid={images[1].node.childImageSharp.fluid} style={{flexBasis: '50%'}} />
-                    {final(images)}
-                </div>
-            </div>
-        )
+        const initialStep = i * 3
+        const images = data.allFile.edges.slice(initialStep, initialStep + 3)
+        elements.push(buildRow(images))
     }
     return (
         <Layout seo={seo}>
@@ -33,9 +28,19 @@ const NotTech = ({ data }: { data: NotTechQuery }) => {
     )
 }
 
+function buildRow(images: import("/Users/amlcurran/Projects/website/src/models/graphql").Edge<any>[]): JSX.Element {
+    return <div style={{ display: "flex", position: "relative" }}>
+        <Img fluid={images[0].node.childImageSharp.fluid} style={{ flexBasis: '60%', ...imageStyle }} />
+        <div style={{ display: "flex", flexDirection: "column", flexBasis: '40%', ...imageStyle }}>
+            <Img fluid={images[1].node.childImageSharp.fluid} style={{ flexBasis: '50%', ...imageStyle }} />
+            {final(images)}
+        </div>
+    </div>;
+}
+
 function final(images: any): JSX.Element | null {
     if (images[2]) {
-        return <Img fluid={images[2].node.childImageSharp.fluid} style={{flexBasis: '50%'}} />                 
+        return <Img fluid={images[2].node.childImageSharp.fluid} style={{flexBasis: '50%', ...imageStyle}} />                 
     }
     return null;
 }
