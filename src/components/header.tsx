f21eeb4485da66b9536bb12cc9808ba682a5a2dd
amlcurran <aml.curran@gmail.com>
2019-04-21 22:31:55 +0200

    Adds CV and contact to main header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 import React, { CSSProperties } from "react"
 import { contentStyle } from "./layout"
 import Styling from "./styling";
@@ -9,12 +9,17 @@ const linkHostStyle: CSSProperties = {
 }
 
 const myStyle: CSSProperties = {
-  fontSize: 32,
   paddingTop: 12,
   paddingBottom: 12,
+  display: 'flex'
+}
+
+const myLinkStyle: CSSProperties = {
+  fontSize: 32,
   fontWeight: 700,
   fontFamily: 'Raleway, sans-serif',
-  letterSpacing: '0.15rem'
+  letterSpacing: '0.15rem',
+  display: 'flex',
 }
 
 const linkHostInnerStyle: CSSProperties = {
@@ -30,18 +35,16 @@ const linkInnerStyle: CSSProperties = {
   textAlign: "center",
   fontFamily: 'Raleway, sans-serif',
   letterSpacing: '0.15rem',
-  minWidth: 160
-}
-
-const externalLinkStyle: CSSProperties = {
-  flexGrow: 1,
-  ...linkInnerStyle
+  minWidth: 160,
+  textTransform: 'uppercase',
+  fontWeight: 700,
+  textDecoration: 'none'
 }
 
 interface HeaderLinkProps {
   to: string
   text: string
-  current: Location
+  current: Location | null
 }
 
 const HeaderLink = ({ to, text, current }: HeaderLinkProps) => (
@@ -50,7 +53,7 @@ const HeaderLink = ({ to, text, current }: HeaderLinkProps) => (
   </Link>
 )
 
-function dotIfCurrent(to: string, current: Location): string {
+function dotIfCurrent(to: string, current: Location | null): string {
   if (current && (current.pathname == to || current.pathname == to + "/")) {
     return "● "
   } else {
@@ -58,24 +61,45 @@ function dotIfCurrent(to: string, current: Location): string {
   }
 }
 
+const contactButton: CSSProperties = {
+  padding: '8px 24px',
+  marginLeft: 16,
+  borderRadius: 24,
+  backgroundColor: Styling.white,
+  color: Styling.primaryColor,
+  ...linkInnerStyle,
+  minWidth: 0
+}
+
 const Header = () => {
-  const location = typeof window !== `undefined` ? window.location : null
-  return (
-    <header>
-      <div style={{ ...contentStyle, ...myStyle }}>
-        <Link to="/" style={{color: Styling.lightTextColor}}>Hey, I'm Alex Curran.</Link>
-      </div>
-      <div style={linkHostStyle} className="emphasisBox">
-        <nav style={{ ...contentStyle, ...linkHostInnerStyle }}>
-          <HeaderLink to="/portfolio" text="Portfolio" current={location} />
-          <HeaderLink to="/talks" text="Talks" current={location} />
-          <HeaderLink to="/side-projects" text="On the side" current={location} />
-          <HeaderLink to="/articles" current={location} text="Articles" />
-          <HeaderLink to="/not-tech" text="Not tech" current={location} />
-        </nav>
-      </div>
-    </header>
-  )
+  return <StaticQuery
+    query={graphql`
+     query{file(name: {eq: "CV"}) {
+      publicURL
+      name
+    }}
+  `}
+    render={(query) => {
+      const location = typeof window !== `undefined` ? window.location : null
+      return (
+        <header>
+          <div style={{ ...contentStyle, ...myStyle }}>
+            <Link to="/" style={{color: Styling.lightTextColor, flexGrow: 1, ...myLinkStyle}}>Hey, I'm Alex Curran.</Link>
+            <a href={query.file.publicURL} style={contactButton} className="emphasisBox">CV</a>
+            <a href="mailto:aml.curran+website@gmail.com" style={contactButton} className="emphasisBox">Contact</a>
+          </div>
+          <div style={linkHostStyle} className="emphasisBox">
+            <nav style={{ ...contentStyle, ...linkHostInnerStyle }}>
+              <HeaderLink to="/portfolio" text="Portfolio" current={location} />
+              <HeaderLink to="/talks" text="Talks" current={location} />
+              <HeaderLink to="/side-projects" text="On the side" current={location} />
+              <HeaderLink to="/articles" current={location} text="Articles" />
+              <HeaderLink to="/not-tech" text="Not tech" current={location} />
+            </nav>
+          </div>
+        </header>
+      )}}
+  />
 }
 
 export default Header
