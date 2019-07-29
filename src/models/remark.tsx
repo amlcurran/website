836572167eec1fd2a50883c13d7bfa107617affe
amlcurran <aml.curran@gmail.com>
2019-07-29 14:22:18 +0200

    Adds single blog entry

--- src/models/remark.tsx
+++ src/models/remark.tsx
@@ -5,6 +5,7 @@ export interface Frontmatter {
 export interface MarkdownRemark<FrontmatterType extends Frontmatter> {
     html: string
     excerpt?: string
+    timeToRead?: number
     id: string
     frontmatter: FrontmatterType
 }
---
+++ src/pages/articles/managing-stress.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import { graphql } from "gatsby";
+import { MarkdownRemark } from "../../models/remark";
+
+interface ArticleFrontmatter {
+    title: string
+}
+
+interface ArticleQuery {
+    markdownRemark: MarkdownRemark<ArticleFrontmatter>
+}
+
+const Article = ({ data }: { data: ArticleQuery }) => {
+    const seo = <SEO title={data.markdownRemark.frontmatter.title} keywords={[`articles`, `blog`, `vlog`, `tech`, `thoughts`]} description="Articles and piece I've written" key="SEO" />
+    return (
+        <Layout seo={seo}>
+            <h5>{data.markdownRemark.timeToRead + " minutes to read"}</h5>
+            <h1>{data.markdownRemark.frontmatter.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}} />
+        </Layout>
+    )
+}
+
+export const query = graphql`
+  query {
+    markdownRemark(fileAbsolutePath: { glob: "**/articles/managing-stress.md" }) {
+        html
+        id
+        timeToRead
+        frontmatter {
+            title
+            date
+        }
+    }
+  }  
+`
+
+export default Article
