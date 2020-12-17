6c4d3059d512082a415e3837f99019d9112701c9
amlcurran <aml.curran@gmail.com>
2020-12-17 21:08:44 +0000

    Update for dark mode

--- src/pages/articles/managing-stress.tsx
+++ src/pages/articles/managing-stress.tsx
@@ -17,7 +17,7 @@ interface ArticleQuery {
 const Article = ({ data }: { data: ArticleQuery }) => {
     const seo = <SEO title={data.markdownRemark.frontmatter.title} keywords={[`articles`, `blog`, `vlog`, `tech`, `thoughts`]} description="Articles and piece I've written" key="SEO" />
     return (
-        <Layout seo={seo} style={{backgroundColor: 'white', paddingTop: 16}}>
+        <Layout seo={seo} style={{paddingTop: 16}}>
             <article>
             <h4>{data.markdownRemark.timeToRead + " minutes to read  ● "}<time>{data.markdownRemark.frontmatter.date}</time></h4>
             <h1>{data.markdownRemark.frontmatter.title}</h1>
