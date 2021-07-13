24dc3ae23407c58b53e95f6c2a1d5b305ad244df
amlcurran <aml.curran@gmail.com>
2021-07-13 22:46:37 +0100

    Add some more style

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -51,7 +51,7 @@ function asArticle(edge: Edge<MarkdownRemark<ArticleFrontmatter>>, data: Article
     date={edge.node.frontmatter.date}
     with={""}
     link={`/articles/${edge.node.frontmatter.slug}`}
-    image={<Img fluid={image.childImageSharp.fluid} style={{ borderRadius: 8 }} />}
+    image={<Img fluid={image.childImageSharp.fluid} className="article-image" />}
     imageSize={'large'}
     html={edge.node.excerpt || ""}
     url={`/articles/${edge.node.frontmatter.slug}`} />
