7b6cc6efedf9e8b59f1215ef038b0be7dacf8126
amlcurran <aml.curran@gmail.com>
2021-11-14 19:14:43 +0000

    Fix the previous links

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -10,13 +10,24 @@ interface ArticleQuery {
     markdownRemark: MarkdownRemark<ArticleFrontmatter>
 }
 
+function previousPlace(previousOpenGraph: OpenGraphProperties): string {
+    if (previousOpenGraph.ogUrl?.includes("freetrade")) {
+        return "Freetrade blog"
+    } else if (previousOpenGraph.ogUrl?.includes("novoda")) {
+        return "Novoda Insights"
+    } else {
+        console.error(`Not sure where this previous URL is ${previousOpenGraph.ogUrl}`)
+        return ""
+    }
+}
+
 function previouslyOnCard(previousOpenGraph: OpenGraphProperties | undefined, previous: string | undefined): JSX.Element {
     if (previousOpenGraph && previousOpenGraph.ogImage.url) {
         return <>
             <div className="tiny-card">
                 <img src={previousOpenGraph.ogImage.url} />
                 <div style={{ paddingTop: 16, paddingBottom: 16 }}>
-                    <i>Previously posted on the <a href={previous}>Freetrade blog</a>:</i>
+                    <i>Previously posted on <a href={previous}>{previousPlace(previousOpenGraph)}</a>:</i>
                     <h4 style={{ marginTop: 8 }}>{previousOpenGraph.ogTitle}</h4>
                     <div className="tiny-card-description">{previousOpenGraph.ogDescription}</div>
                 </div>
