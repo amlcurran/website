e2559081841ef5529c008f4f564db8abd0184f15
Alex Curran <aml.curran@gmail.com>
2022-04-16 15:13:47 +0100

    Start to modularise Sass

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,17 +1,12 @@
-import React, {CSSProperties} from "react"
-import Styling from "./styling"
+import React from "react"
+import "./chips.scss"
 
 interface ChipProps {
     text: string
-    style?: CSSProperties
 }
 
 export const Chip = (props: ChipProps) => {
     return (
-        <div style={{backgroundColor: 'var(--primary-hover)',
-            marginRight: 5,
-            padding: '5px 10px',
-            borderRadius: 20,
-            color: Styling.primaryColor}}>{props.text}</div>
+        <div className="chip">{props.text}</div>
     )
 }
